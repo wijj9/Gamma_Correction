8fc60b4a95ddb29431db1c2dd78fe547b49c4b6c
Wajih <ge47jez@tum.de>
2024-10-11 03:08:39 +0200

    Project Restructure: forth iteration

--- server/app.py
+++ server/app.py
@@ -122,4 +122,4 @@ def download_image(filename):
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
